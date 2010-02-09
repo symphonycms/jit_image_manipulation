1964f38ae683489f1a1015934e15e6b1d6887c55
Nils Hörrmann <post@nilshoerrmann.de>
2010-02-09 18:11:09 +0100

    Adjust German localisation

--- lang/lang.de.php
+++ lang/lang.de.php
@@ -7,51 +7,49 @@
 			'email' => 'post@nilshoerrmann.de',
 			'website' => 'http://www.nilshoerrmann.de'
 		),
-		'release-date' => '2009-12-29'
+		'release-date' => '2010-02-06',
 	);
-	
-	
-	/*
-	 * EXTENSION: JIT Image Manipulation
-	 * Localisation strings
+
+
+	/**
+	 * JIT Image Manipulation
 	 */
-
 	$dictionary = array(
-
-		'Error reading external image <code>%s</code>. Please check the URI.' => 
-		'Fehler beim Lesen des externen Bildes <code>%s</code>. Bitte überprüfen Sie die URI.',
-
-		'Error writing to temporary file <code>%s</code>.' => 
-		'Fehler beim Schreiben der temporären Daten <code>%s</code>.',
-
-		'Error loading image <code>%s</code>. Check it exists and is readable.' => 
-		'Fehler beim Laden des Bildes <code>%s</code>. Überprüfen Sie, ob es tatsächlich exisitiert und lesbar ist.',
 
 		'Cannot load CMYK JPG Images' => 
 		'JPG-Bilder in CMYK können nicht geladen werden',
 
-		'Unsupported image type. Supported types: GIF, JPEG and PNG' => 
-		'Bildtyp nicht unterstützt. Unterstützte Typen: GIF, JPEG und PNG',
-
-		'Invalid image resource supplied' => 
-		'Diese Bilddatei ist ungültig',
-
 		'Error: Connecting to that external site is not permitted.' => 
 		'Fehler: Eine Verbindung mit dieser externen Seite ist nicht gestattet.',
-
-		'Image <code>%s</code> could not be found.' => 
-		'Das Bild <code>%s</code> konnte nicht gefunden werden.',
 
 		'Error generating image' => 
 		'Fehler bei Erzeugen des Bildes',
 
+		'Error loading image <code>%s</code>. Check it exists and is readable.' => 
+		'Fehler beim Laden des Bildes <code>%s</code>. Überprüfen Sie, ob es tatsächlich exisitiert und lesbar ist.',
+
+		'Error reading external image <code>%s</code>. Please check the URI.' => 
+		'Fehler beim Einlesen des externen Bildes <code>%s</code>. Bitte überprüfen Sie die URI.',
+
+		'Error writing to temporary file <code>%s</code>.' => 
+		'Fehler beim Schreiben der temporären Daten <code>%s</code>.',
+
+		'Image <code>%s</code> could not be found.' => 
+		'Das Bild <code>%s</code> konnte nicht gefunden werden.',
+
+		'Invalid image resource supplied' => 
+		'Diese Bilddatei ist ungültig',
+
 		'JIT Image Manipulation' => 
-		'JIT-Bildmanipulation',
+		'Echtzeitbildbearbeitung',
+
+		'Leave empty to disable external linking. Single rule per line. Add * at end for wild card matching.' => 
+		'Lassen Sie dieses Feld frei, wenn Sie externe Verweise unterbinden wollen. Eine Regel pro Zeile. Verwenden Sie * als Maskenzeichen.',
 
 		'Trusted Sites' => 
 		'Vertrauenswürdige Seiten',
 
-		'Leave empty to disable external linking. Single rule per line. Add * at end for wild card matching.' => 
-		'Lassen Sie dieses Feld frei, wenn Sie externe Verweise unterbinden wollen. Eine Regel pro Zeile. Verwenden Sie * als Maskenzeichen.'
+		'Unsupported image type. Supported types: GIF, JPEG and PNG' => 
+		'Bildtyp nicht unterstützt. Unterstützte Typen: GIF, JPEG und PNG',
 
 	);
