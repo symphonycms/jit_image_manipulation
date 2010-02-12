30639accea860c84d40f784afbdcb57fab15193d
Pierre Bertet <bonjour@pierrebertet.net>
2010-02-12 14:12:32 +0100

    French translation

---
+++ lang/lang.fr.php
@@ -0,0 +1,55 @@
+<?php
+
+	$about = array(
+		'name' => 'French',
+		'author' => array(
+			'name' => 'Pierre',
+			'email' => 'bonjour@pierrebertet.net',
+			'website' => 'http://www.pierrebertet.net'
+		),
+		'release-date' => '2010-02-12',
+	);
+
+
+	/**
+	 * JIT Image Manipulation
+	 */
+	$dictionary = array(
+
+		'Cannot load CMYK JPG Images' => 
+		'Impossible de charger des images CMJN',
+
+		'Error: Connecting to that external site is not permitted.' => 
+		'Erreur : La connexion à ce site externe n’est pas autorisée.',
+
+		'Error generating image' => 
+		'Erreur lors de la génération de l’image',
+
+		'Error loading image <code>%s</code>. Check it exists and is readable.' => 
+		'Erreur de chargement de l’image <code>%s</code>. Vérifiez qu’elle existe et ses permissions en lecture.',
+
+		'Error reading external image <code>%s</code>. Please check the URI.' => 
+		'Erreur de lecture de l’image externe <code>%s</code>. Veuillez vérifier l’URI.',
+
+		'Error writing to temporary file <code>%s</code>.' => 
+		'Erreur d’écriture du fichier temporaire <code>%s</code>.',
+
+		'Image <code>%s</code> could not be found.' => 
+		'L’image <code>%s</code> est introuvable.',
+
+		'Invalid image resource supplied' => 
+		'L’image fournie n’est pas valide',
+
+		'JIT Image Manipulation' => 
+		'Manipulation d’images à la volée',
+
+		'Leave empty to disable external linking. Single rule per line. Add * at end for wild card matching.' => 
+		'Laissez ce champ vide pour désactiver les liens exernes. Une seule règle par ligne. Utilisez le caractère * en fin de ligne pour autoriser plusieurs URLs.',
+
+		'Trusted Sites' => 
+		'Sites de confiance',
+
+		'Unsupported image type. Supported types: GIF, JPEG and PNG' => 
+		'Type d’image non supporté. Types supportés : GIF, JPEG et PNG',
+
+	);
