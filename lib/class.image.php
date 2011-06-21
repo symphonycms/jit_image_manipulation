07d4782052d8363031287f1fe3c5fa3026108767
Brendan Abbott <brendan@randb.com.au>
2011-06-21 18:36:51 +1000

    Document the Image class

--- lib/class.image.php
+++ lib/class.image.php
@@ -14,7 +14,9 @@
 		}
 
 		public function __destruct(){
-			@imagedestroy($this->_resource);
+			if(is_resource($this->_resource)) {
+				imagedestroy($this->_resource);
+			}
 		}
 
 		public function Resource(){
@@ -25,6 +27,19 @@
 			return $this->_meta;
 		}
 
+		/**
+		 * This function will attempt to load an image from a remote URL using
+		 * CURL. If CURL is not available, `file_get_contents` will attempt to resolve
+		 * the given `$uri`. The remote image will be saved into PHP's temporary directory
+		 * as determined by `sys_get_temp_dir`. Once the remote image has been
+		 * saved to the temp directory, it's path will be passed to `Image::load`
+		 * to return an instance of this `Image` class. If the file cannot be found
+		 * an Exception is thrown.
+		 *
+		 * @param string $uri
+		 *  The URL of the external image to load.
+		 * @return Image
+		 */
 		public static function loadExternal($uri){
 			if(function_exists('curl_init')){
 				$ch = curl_init();
@@ -49,6 +64,16 @@
 			return self::load($dest);
 		}
 
+		/**
+		 * Given a path to an image, `$image`, this function will verify it's
+		 * existence, and generate a resource for use with PHP's image functions
+		 * based off the file's type (.gif, .jpg, .png).
+		 * Images must be RGB, CMYK jpg's are not supported due to GD limitations.
+		 *
+		 * @param string $image
+		 *  The path to the file
+		 * @return Image
+		 */
 		public static function load($image){
 			if(!is_file($image) || !is_readable($image)){
 				throw new Exception(__('Error loading image <code>%s</code>. Check it exists and is readable.', array($image)));
@@ -64,7 +89,6 @@
 
 				// JPEG
 				case IMAGETYPE_JPEG:
-
 					if($meta->channels <= 3){
 						$resource = imagecreatefromjpeg($image);
 					}
@@ -93,6 +117,14 @@
 			return $obj;
 		}
 
+		/**
+		 * Given a path to a file, this function will attempt to find out the
+		 * dimensions, type and channel information using `getimagesize`.
+		 *
+		 * @link http://www.php.net/manual/en/function.getimagesize.php
+		 * @param string $file
+		 *  The path to the image.
+		 */
 		public static function getMetaInformation($file){
 			if(!$array = @getimagesize($file)) return false;
 
@@ -106,19 +138,34 @@
 			return (object)$meta;
 		}
 
-		public static function renderOutputHeaders($output, $dest=NULL){
-			header('Content-Type: ' . image_type_to_mime_type($output));
+		/**
+		 * Given string representing the type return by `getMetaInformation`,
+		 * this function will generate the correct Content-Type header using
+		 * `image_type_to_mime_type` function.
+		 *
+		 * @see getMetaInformation()
+		 * @link http://php.net/manual/en/function.image-type-to-mime-type.php
+		 * @link http://www.php.net/manual/en/image.constants.php
+		 * @param integer $type
+		 *  One of the IMAGETYPE constants
+		 * @param string $destination
+		 *  The destination of the image. This defaults to null, if provided,
+		 *  this function will prompt the user to download the image rather
+		 *  than display it inline
+		 */
+		public static function renderOutputHeaders($type, $destination=NULL){
+			header('Content-Type: ' . image_type_to_mime_type($type));
 
-			if(!$dest) return;
+			if(is_null($destination)) return;
 
 			// Try to remove old extension
-			$ext = strrchr($dest, '.');
+			$ext = strrchr($destination, '.');
 			if($ext !== false){
-				$dest = substr($dest, 0, -strlen($ext));
+				$destination = substr($destination, 0, -strlen($ext));
 			}
 
 			header('Expires: ' . gmdate('D, d M Y H:i:s') . ' GMT');
-			header("Content-Disposition: inline; filename=$dest" . image_type_to_extension($output));
+			header("Content-Disposition: inline; filename=$destination" . image_type_to_extension($type));
 			header('Pragma: no-cache');
 		}
 
@@ -202,14 +249,66 @@
 			return $retVal;
 		}
 
+		/**
+		 * Accessor to return of the height of the given `$res`
+		 *
+		 * @param resource $res
+		 * @return integer
+		 *  The height of the image in pixels, false otherwise
+		 */
 		public static function height($res){
 			return imagesy($res);
 		}
 
+		/**
+		 * Accessor to return of the width of the given `$res`
+		 *
+		 * @param resource $res
+		 * @return integer
+		 *  The width of the image in pixels, false otherwise
+		 */
 		public static function width($res){
 			return imagesx($res);
 		}
 
+		/**
+		 * Given a filter name, this function will attempt to load the filter
+		 * from the `/filters` folder and call it's `run` method. The JIT core
+		 * provides filters for 'crop', 'resize' and 'scale'.
+		 *
+		 * @param string $filter
+		 *  The filter name
+		 * @param array $args
+		 *  The arguments to pass to the filter's `run()` method
+		 * @return boolean
+		 */
+		public function applyFilter($filter = null, array $args = array()) {
+			if(is_null($filter) || !is_file("filters/filter.{$filter}.php")) return false;
+
+			require_once("filters/filter.{$filter}.php");
+
+			array_unshift($args, $this->_resource);
+
+			$this->_resource = call_user_func_array(array(sprintf('Filter%s', ucfirst($filter)), 'run'), $args);
+
+			return true;
+		}
+
+		/**
+		 * The function takes optional `$quality`, `$interlacing` and `$output`
+		 * parameters to return an image resource after it has been processed
+		 * using `applyFilter`.
+		 *
+		 * @param integer $quality
+		 *  Range of 1-100, if not provided, uses `Image::DEFAULT_QUALITY`
+		 * @param boolean $interlacing
+		 *  If true, the resulting image will be interlaced, otherwise it won't.
+		 *  By default uses the value of `Image::DEFAULT_INTERLACE`
+		 * @param IMAGETYPE_xxx $output
+		 *  A IMAGETYPE constant of the image's type, by default this is null
+		 *  which will attempt to get the constant using the `$this->Meta` accessor
+		 * @return resource
+		 */
 		public function display($quality = Image::DEFAULT_QUALITY, $interlacing = Image::DEFAULT_INTERLACE, $output = null) {
 			if(!$output) $output = $this->Meta()->type; //DEFAULT_OUTPUT_TYPE;
 
@@ -218,17 +317,48 @@
 			if(isset($this->_image) && is_resource($this->_image)) {
 				return $this->_image;
 			}
-			else return self::__render(NULL, $quality, $output, $interlacing);
+			else return self::__render(NULL, $quality, $interlacing, $output);
 		}
 
+		/**
+		 * This function will attempt to save an image at a desired destination.
+		 *
+		 * @param string $dest
+		 *  The path to save the image at
+		 * @param integer $quality
+		 *  Range of 1-100, if not provided, uses `Image::DEFAULT_QUALITY`
+		 * @param boolean $interlacing
+		 *  If true, the resulting image will be interlaced, otherwise it won't.
+		 *  By default uses the value of `Image::DEFAULT_INTERLACE`
+		 * @param IMAGETYPE_xxx $output
+		 *  A IMAGETYPE constant of the image's type, by default this is null
+		 *  which will attempt to get the constant using the `$this->Meta` accessor
+		 * @return boolean
+		 */
 		public function save($dest, $quality = Image::DEFAULT_QUALITY, $interlacing = Image::DEFAULT_INTERLACE, $output = null) {
 			if(!$output) $output = $this->Meta()->type; //DEFAULT_OUTPUT_TYPE;
 
-			$this->_image = self::__render($dest, $quality, $output, $interlacing);
+			$this->_image = self::__render($dest, $quality, $interlacing, $output);
 			return $this->_image;
 		}
 
-		private function __render($dest, $quality = Image::DEFAULT_QUALITY, $output, $interlacing=false){
+		/**
+		 * Renders the `$this->_resource` using the PHP image functions to save the
+		 * image at a location specified by `$dest`.
+		 *
+		 * @param string $dest
+		 *  The path to save the image at
+		 * @param integer $quality
+		 *  Range of 1-100, if not provided, uses `Image::DEFAULT_QUALITY`
+		 * @param boolean $interlacing
+		 *  If true, the resulting image will be interlaced, otherwise it won't.
+		 *  By default uses the value of `Image::DEFAULT_INTERLACE`
+		 * @param IMAGETYPE_xxx $output
+		 *  A IMAGETYPE constant of the image's type, by default this is null
+		 *  which will attempt to get the constant using the `$this->Meta` accessor
+		 * @return boolean
+		 */
+		private function __render($dest, $quality = Image::DEFAULT_QUALITY, $interlacing = Image::DEFAULT_INTERLACE, $output = null){
 			if(!is_resource($this->_resource)) {
 				throw new Exception(__('Invalid image resource supplied'));
 			}
@@ -254,16 +384,6 @@
 			}
 
 			return false;
-		}
-
-		public function applyFilter($filter, array $args = array()) {
-			require_once("filters/filter.{$filter}.php");
-
-			array_unshift($args, $this->_resource);
-
-			$this->_resource = call_user_func_array(array(sprintf('Filter%s', ucfirst($filter)), 'run'), $args);
-
-			return true;
 		}
 
 	}
