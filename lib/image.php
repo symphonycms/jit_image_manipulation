1b4ba22771b62dfbd0f44f918fd5e5ffc8cbe650
michael-e <info@michael-eichelsdoerfer.de>
2011-08-04 15:56:09 +0200

    Inventing '$original_file' variable. This will be very useful a bit later (because the image_path may change).

--- lib/image.php
+++ lib/image.php
@@ -193,6 +193,7 @@
 	// If CACHING is enabled, check to see that the cached file is still valid.
 	if(CACHING === true){
 		$cache_file = sprintf('%s/%s_%s', CACHE, md5($_REQUEST['param'] . intval($settings['image']['quality'])), basename($image_path));
+		$original_file = $image_path;
 
 		// Cache has expired or doesn't exist
 		if(is_file($cache_file) && (filemtime($cache_file) < $last_modified)){
