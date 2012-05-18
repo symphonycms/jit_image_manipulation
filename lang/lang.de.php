53cfb9d9ac18f992c45e937164211d4f3a65a3fe
Nils Hörrmann <post@nilshoerrmann.de>
2012-05-18 21:19:47 +0200

    Updated German translation

--- lang/lang.de.php
+++ lang/lang.de.php
@@ -7,12 +7,11 @@
 			'email' => 'post@nilshoerrmann.de',
 			'website' => 'http://www.nilshoerrmann.de'
 		),
-		'release-date' => '2010-02-06',
+		'release-date' => '2012-05-18'
 	);
 
-
 	/**
-	 * JIT Image Manipulation
+	 * Just In Time (JIT) Image Manipulation
 	 */
 	$dictionary = array(
 
@@ -24,5 +23,113 @@
 
 		'Trusted Sites' => 
 		'Vertrauenswürdige Seiten',
+
+		'An error occurred while installing %s. %s' => 
+		'Bei der Installation von %s ist ein Fehler aufgetreten. %s',
+
+		'A recipe with this regular expression already exists.' => 
+		'Ein Rezept mit diesem regulären Ausdruck existiert bereits',
+
+		'A recipe with this handle already exists. All handles must be unique.' => 
+		'Ein Rezept mit diesem Bezeichner existiert bereits. Bezeichner müssen eindeutig sein.',
+
+		'Direct display' => 
+		'Direktanzeige',
+
+		'Resize' => 
+		'Größenanpassung',
+
+		'Crop to Fill' => 
+		'Vollflächiger Zuschnitt',
+
+		'Resize Canvas' => 
+		'Größenanpassung der Bildfläche',
+
+		'Resize to Fit' => 
+		'Passgenaue Größenanpassung',
+
+		'Custom' => 
+		'Benutzerdefiniert',
+
+		'Top left' => 
+		'oben links',
+
+		'Top center' => 
+		'oben Mitte',
+
+		'Top right' => 
+		'oben rechts',
+
+		'Center left' => 
+		'Mitte links',
+
+		'Center' => 
+		'Mitte',
+
+		'Center right' => 
+		'Mitte rechts',
+
+		'Bottom left' => 
+		'unten links',
+
+		'Bottom center' => 
+		'unten Mitte',
+
+		'Bottom right' => 
+		'unten rechts',
+
+		'New Recipe' => 
+		'Neues Rezept',
+
+		'Regular Expression' => 
+		'Regulärer Ausdruck',
+
+		'Width' => 
+		'Breite',
+
+		'Height' => 
+		'Höhe',
+
+		'Position' => 
+		'Position',
+
+		'Background Color' => 
+		'Hintergrundfarbe',
+
+		'JIT Parameter' => 
+		'Bildparameter',
+
+		'Image quality' => 
+		'Bildqualität',
+
+		'External Image' => 
+		'Externe Bilder',
+
+		'The JIT configuration folder, %s, does not exist. You will not be able to save recipes and trusted sites.' => 
+		'Das Konfigurationsverzeichnis, %s, existiert nicht. Sie können keine Rezepte oder vertrauenswürdigen Seiten speichern.',
+
+		'Recipes are named rules for the JIT settings.' => 
+		'Rezepte sind angepasste Regeln für die Bildbearbeitung',
+
+		'Recipes' => 
+		'Rezepte',
+
+		'Add recipe' => 
+		'Rezept hinzufügen',
+
+		'Remove recipe' => 
+		'Rezept entfernen',
+
+		'Disable dynamic URLs and use named recipes only' => 
+		'Deaktiviere dynamische URLs und verwende ausschließlich angelegte Rezepte',
+
+		'An error occurred while saving the JIT trusted sites. Make sure the trusted sites file, %s, exists and is writable.' => 
+		'Beim Speichern der vertrauenswürdigen Seiten ist eine Fehler aufgetreten. Stellen Sie sicher, dass die Datei mit den vertrauenswürdigen Seiten, %s, existiert und schreibbar ist.',
+
+		'An error occurred while saving the JIT recipes. Make sure the recipes file, %s, exists and is writable.' => 
+		'Beim Speichern der Rezepte ist ein Fehler aufgetreten. Stellen Sie sicher, dass das Rezept %s existiert und schreibbar ist.',
+
+		'An error occurred while saving the JIT recipes.' => 
+		'Beim Speichern der Rezepte ist ein Fehleraufgetreten.',
 
 	);
