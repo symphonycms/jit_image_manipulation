9184553f5965f373d56e72e0d65307b607a4af5d
Nils Hörrmann <github@nilshoerrmann.de>
2013-09-18 13:59:24 +0200

    Update lang.de.php

--- lang/lang.de.php
+++ lang/lang.de.php
@@ -7,7 +7,7 @@
 			'email' => 'post@nilshoerrmann.de',
 			'website' => 'http://www.nilshoerrmann.de'
 		),
-		'release-date' => '2012-05-18'
+		'release-date' => '2013-09-18'
 	);
 
 	/**
@@ -15,32 +15,98 @@
 	 */
 	$dictionary = array(
 
-		'JIT Image Manipulation' => 
-		'Echtzeitbildbearbeitung',
-
-		'Leave empty to disable external linking. Single rule per line. Add * at end for wild card matching.' => 
-		'Lassen Sie dieses Feld frei, wenn Sie externe Verweise unterbinden wollen. Eine Regel pro Zeile. Verwenden Sie * als Maskenzeichen.',
-
-		'Trusted Sites' => 
-		'Vertrauenswürdige Seiten',
-
-		'An error occurred while installing %s. %s' => 
-		'Bei der Installation von %s ist ein Fehler aufgetreten. %s',
+		'A recipe with this handle already exists. All handles must be unique.' => 
+		'Ein Rezept mit diesem Bezeichner existiert bereits. Bezeichner müssen eindeutig sein.',
 
 		'A recipe with this regular expression already exists.' => 
 		'Ein Rezept mit diesem regulären Ausdruck existiert bereits',
 
-		'A recipe with this handle already exists. All handles must be unique.' => 
-		'Ein Rezept mit diesem Bezeichner existiert bereits. Bezeichner müssen eindeutig sein.',
+		'Add recipe' => 
+		'Rezept hinzufügen',
+
+		'An error occurred while installing %s. %s' => 
+		'Bei der Installation von %s ist ein Fehler aufgetreten. %s',
+
+		'An error occurred while saving the JIT recipes.' => 
+		'Beim Speichern der Rezepte ist ein Fehleraufgetreten.',
+
+		'An error occurred while saving the JIT recipes. Make sure the recipes file, %s, exists and is writable.' => 
+		'Beim Speichern der Rezepte ist ein Fehler aufgetreten. Stellen Sie sicher, dass das Rezept %s existiert und schreibbar ist.',
+
+		'An error occurred while saving the JIT trusted sites. Make sure the trusted sites file, %s, exists and is writable.' => 
+		'Beim Speichern der vertrauenswürdigen Seiten ist eine Fehler aufgetreten. Stellen Sie sicher, dass die Datei mit den vertrauenswürdigen Seiten, %s, existiert und schreibbar ist.',
+
+		'Background Color' => 
+		'Hintergrundfarbe',
+
+		'Bottom center' => 
+		'unten Mitte',
+
+		'Bottom left' => 
+		'unten links',
+
+		'Bottom right' => 
+		'unten rechts',
+
+		'Center' => 
+		'Mitte',
+
+		'Center left' => 
+		'Mitte links',
+
+		'Center right' => 
+		'Mitte rechts',
+
+		'Crop to Fill' => 
+		'Vollflächiger Zuschnitt',
+
+		'Custom' => 
+		'Benutzerdefiniert',
 
 		'Direct display' => 
 		'Direktanzeige',
 
+		'Disable dynamic URLs and use named recipes only' => 
+		'Deaktiviere dynamische URLs und verwende ausschließlich angelegte Rezepte',
+
+		'External Image' => 
+		'Externe Bilder',
+
+		'Height' => 
+		'Höhe',
+
+		'Image quality' => 
+		'Bildqualität',
+
+		'JIT Image Manipulation' => 
+		'Echtzeitbildbearbeitung',
+
+		'JIT Parameter' => 
+		'Bildparameter',
+
+		'Leave empty to disable external linking. Single rule per line. Add * at end for wild card matching.' => 
+		'Lassen Sie dieses Feld frei, wenn Sie externe Verweise unterbinden wollen. Eine Regel pro Zeile. Verwenden Sie * als Maskenzeichen.',
+
+		'New Recipe' => 
+		'Neues Rezept',
+
+		'Position' => 
+		'Position',
+
+		'Recipes' => 
+		'Rezepte',
+
+		'Recipes are named rules for the JIT settings.' => 
+		'Rezepte sind angepasste Regeln für die Bildbearbeitung',
+
+		'Regular Expression' => 
+		'Regulärer Ausdruck',
+
+		'Remove recipe' => 
+		'Rezept entfernen',
+
 		'Resize' => 
 		'Größenanpassung',
-
-		'Crop to Fill' => 
-		'Vollflächiger Zuschnitt',
 
 		'Resize Canvas' => 
 		'Größenanpassung der Bildfläche',
@@ -48,88 +114,28 @@
 		'Resize to Fit' => 
 		'Passgenaue Größenanpassung',
 
-		'Custom' => 
-		'Benutzerdefiniert',
-
-		'Top left' => 
-		'oben links',
+		'The JIT configuration folder, %s, does not exist. You will not be able to save recipes and trusted sites.' => 
+		'Das Konfigurationsverzeichnis, %s, existiert nicht. Sie können keine Rezepte oder vertrauenswürdigen Seiten speichern.',
 
 		'Top center' => 
 		'oben Mitte',
 
+		'Top left' => 
+		'oben links',
+
 		'Top right' => 
 		'oben rechts',
 
-		'Center left' => 
-		'Mitte links',
-
-		'Center' => 
-		'Mitte',
-
-		'Center right' => 
-		'Mitte rechts',
-
-		'Bottom left' => 
-		'unten links',
-
-		'Bottom center' => 
-		'unten Mitte',
-
-		'Bottom right' => 
-		'unten rechts',
-
-		'New Recipe' => 
-		'Neues Rezept',
-
-		'Regular Expression' => 
-		'Regulärer Ausdruck',
+		'Trusted Sites' => 
+		'Vertrauenswürdige Seiten',
 
 		'Width' => 
 		'Breite',
 
-		'Height' => 
-		'Höhe',
+		'An error occurred while updating %s. %s' => 
+		'Beim Aktualisieren von $s ist ein Fehler aufgetreten. $s',
 
-		'Position' => 
-		'Position',
-
-		'Background Color' => 
-		'Hintergrundfarbe',
-
-		'JIT Parameter' => 
-		'Bildparameter',
-
-		'Image quality' => 
-		'Bildqualität',
-
-		'External Image' => 
-		'Externe Bilder',
-
-		'The JIT configuration folder, %s, does not exist. You will not be able to save recipes and trusted sites.' => 
-		'Das Konfigurationsverzeichnis, %s, existiert nicht. Sie können keine Rezepte oder vertrauenswürdigen Seiten speichern.',
-
-		'Recipes are named rules for the JIT settings.' => 
-		'Rezepte sind angepasste Regeln für die Bildbearbeitung',
-
-		'Recipes' => 
-		'Rezepte',
-
-		'Add recipe' => 
-		'Rezept hinzufügen',
-
-		'Remove recipe' => 
-		'Rezept entfernen',
-
-		'Disable dynamic URLs and use named recipes only' => 
-		'Deaktiviere dynamische URLs und verwende ausschließlich angelegte Rezepte',
-
-		'An error occurred while saving the JIT trusted sites. Make sure the trusted sites file, %s, exists and is writable.' => 
-		'Beim Speichern der vertrauenswürdigen Seiten ist eine Fehler aufgetreten. Stellen Sie sicher, dass die Datei mit den vertrauenswürdigen Seiten, %s, existiert und schreibbar ist.',
-
-		'An error occurred while saving the JIT recipes. Make sure the recipes file, %s, exists and is writable.' => 
-		'Beim Speichern der Rezepte ist ein Fehler aufgetreten. Stellen Sie sicher, dass das Rezept %s existiert und schreibbar ist.',
-
-		'An error occurred while saving the JIT recipes.' => 
-		'Beim Speichern der Rezepte ist ein Fehleraufgetreten.',
+		'Disable upscaling of images beyond the original size' => 
+		'Das Aufblasen von Bilder über die Originalgröße hinaus verhindern',
 
 	);
