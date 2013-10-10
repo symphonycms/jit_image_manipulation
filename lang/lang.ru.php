c42464faabea0a539ac972638677dae2d1918418
alexbirukov <lord_maloy@mail.ru>
2013-10-10 13:17:27 +0600

    Update Russian language file
    Update Russian language file

--- lang/lang.ru.php
+++ lang/lang.ru.php
@@ -7,7 +7,7 @@
 			'email' => 'info@alexbirukov.ru',
 			'website' => 'http://alexbirukov.ru'
 		),
-		'release-date' => '2012-06-16'
+		'release-date' => '2013-10-10'
 	);
 
 	/**
@@ -15,33 +15,98 @@
 	 */
 	$dictionary = array(
 
-		'JIT Image Manipulation' => 
-		'JIT обработка изображений',
-
-		'Leave empty to disable external linking. Single rule per line. Add * at end for wild card matching.' => 
-		'Оставьте поле пустым, чтобы запретить внешние ссылки. Только одно правило в строке. Используйте * для замены любого символа.',
-
-		'Trusted Sites' => 
-		'Доверенные сайты',
-
-
-		'An error occurred while installing %s. %s' => 
-		'Во время установки произошла ошибка %s. %s',
+		'A recipe with this handle already exists. All handles must be unique.' => 
+		'Правило обработки с указанным обарботчиком уже существует. Все обработчики должны бить уникальны.',
 
 		'A recipe with this regular expression already exists.' => 
 		'Правило обработки с указанным регулярным выражением уже существует.',
 
-		'A recipe with this handle already exists. All handles must be unique.' => 
-		'Правило обработки с указанным обарботчиком уже существует. Все обработчики должны бить уникальны.',
+		'Add recipe' => 
+		'Добавить правило',
+
+		'An error occurred while installing %s. %s' => 
+		'Во время установки произошла ошибка %s. %s',
+
+		'An error occurred while saving the JIT recipes.' => 
+		'Произошла ошибка при сохранении правил обработки JIT.',
+
+		'An error occurred while saving the JIT recipes. Make sure the recipes file, %s, exists and is writable.' => 
+		'При сохранении списка правил обработки JIT произошла ошибка. Удостоверьтесь, что файл %s существует и доступен для записи.',
+
+		'An error occurred while saving the JIT trusted sites. Make sure the trusted sites file, %s, exists and is writable.' => 
+		'При сохранении списка доверенных сайтов JIT произошла ошибка. Удостоверьтесь, что файл %s существует и доступен для записи.',
+
+		'Background Color' => 
+		'Цвет фона',
+
+		'Bottom center' => 
+		'Низ центр',
+
+		'Bottom left' => 
+		'Низ лево',
+
+		'Bottom right' => 
+		'Низ право',
+
+		'Center' => 
+		'Центр',
+
+		'Center left' => 
+		'Центр лево',
+
+		'Center right' => 
+		'Центр право',
+
+		'Crop to Fill' => 
+		'Обрезать по рамке',
+
+		'Custom' => 
+		'Произвольный',
 
 		'Direct display' => 
 		'Без изменений',
 
+		'Disable dynamic URLs and use named recipes only' => 
+		'Отключить динамические URL и использовать только правила обработки',
+
+		'External Image' => 
+		'Внешнее изображение',
+
+		'Height' => 
+		'Высота',
+
+		'Image quality' => 
+		'Качество',
+
+		'JIT Image Manipulation' => 
+		'JIT обработка изображений',
+
+		'JIT Parameter' => 
+		'JIT параметр',
+
+		'Leave empty to disable external linking. Single rule per line. Add * at end for wild card matching.' => 
+		'Оставьте поле пустым, чтобы запретить внешние ссылки. Только одно правило в строке. Используйте * для замены любого символа.',
+
+		'New Recipe' => 
+		'Новое правило обработки',
+
+		'Position' => 
+		'Позиция',
+
+		'Recipes' => 
+		'Правила обработки',
+
+		'Recipes are named rules for the JIT settings.' => 
+		'Правила обработки это именованые настройи работы JIT',
+
+		'Regular Expression' => 
+		'Регулярное выражение',
+
+		'Remove recipe' => 
+		'Удалить правило',
+
 		'Resize' => 
 		'Изменить размер',
-
-		'Crop to Fill' => 
-		'Обрезать по рамке',
 
 		'Resize Canvas' => 
 		'Изменить размер холста',
@@ -49,88 +114,28 @@
 		'Resize to Fit' => 
 		'Изменить размер под рамку',
 
-		'Custom' => 
-		'Произвольный',
-
-		'Top left' => 
-		'Верх лево',
+		'The JIT configuration folder, %s, does not exist. You will not be able to save recipes and trusted sites.' => 
+		'Каталог конфигурации JIT %s не существует. Настройки правил и доверенных сайтов не будут сохранены.',
 
 		'Top center' => 
 		'Верх центр',
 
+		'Top left' => 
+		'Верх лево',
+
 		'Top right' => 
 		'Верх право',
 
-		'Center left' => 
-		'Центр лево',
-
-		'Center' => 
-		'Центр',
-
-		'Center right' => 
-		'Центр право',
-
-		'Bottom left' => 
-		'Низ лево',
-
-		'Bottom center' => 
-		'Низ центр',
-
-		'Bottom right' => 
-		'Низ право',
-
-		'New Recipe' => 
-		'Новое правило обработки',
-
-		'Regular Expression' => 
-		'Регулярное выражение',
+		'Trusted Sites' => 
+		'Доверенные сайты',
 
 		'Width' => 
 		'Ширина',
 
-		'Height' => 
-		'Высота',
+		'An error occurred while updating %s. %s' => 
+		'Произошла ошибка в процессе обновления %s. %s',
 
-		'Position' => 
-		'Позиция',
-
-		'Background Color' => 
-		'Цвет фона',
-
-		'JIT Parameter' => 
-		'JIT параметр',
-
-		'Image quality' => 
-		'Качество',
-
-		'External Image' => 
-		'Внешнее изображение',
-
-		'The JIT configuration folder, %s, does not exist. You will not be able to save recipes and trusted sites.' => 
-		'Каталог конфигурации JIT %s не существует. Настройки правил и доверенных сайтов не будут сохранены.',
-
-		'Recipes are named rules for the JIT settings.' => 
-		'Правила обработки это именованые настройи работы JIT',
-
-		'Recipes' => 
-		'Правила обработки',
-
-		'Add recipe' => 
-		'Добавить правило',
-
-		'Remove recipe' => 
-		'Удалить правило',
-
-		'Disable dynamic URLs and use named recipes only' => 
-		'Отключить динамические URL и использовать только правила обработки',
-
-		'An error occurred while saving the JIT trusted sites. Make sure the trusted sites file, %s, exists and is writable.' => 
-		'При сохранении списка доверенных сайтов JIT произошла ошибка. Удостоверьтесь, что файл %s существует и доступен для записи.',
-
-		'An error occurred while saving the JIT recipes. Make sure the recipes file, %s, exists and is writable.' => 
-		'При сохранении списка правил обработки JIT произошла ошибка. Удостоверьтесь, что файл %s существует и доступен для записи.',
-
-		'An error occurred while saving the JIT recipes.' => 
-		'Произошла ошибка при сохранении правил обработки JIT.',
+		'Disable upscaling of images beyond the original size' => 
+		'Отключить масштабирование изображения за пределы первоначального размера',
 
 	);
