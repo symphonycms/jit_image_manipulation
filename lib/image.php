aac2f3fb4d73ff0836256924cda2136fd99cacba
Nicolas Brassard <nico@nitriques.com>
2014-02-16 13:45:14 -0500

    Added the logic in the image handler for disable_proxy_transform

--- lib/image.php
+++ lib/image.php
@@ -246,11 +246,18 @@
 	if($last_modified) {
 		$last_modified_gmt = gmdate('D, d M Y H:i:s', $last_modified) . ' GMT';
 		$etag = md5($last_modified . $image_path);
+		$cacheControl = 'public';
+		
+		// Add no-transform in order to prevent ISPs to
+		// serve image over http through a compressing proxy
+		// See #79
+		if (Symphony::Configuration()->get('disable_proxy_transform', 'image') == 'yes') {
+			$cacheControl .= ', no-transform';
+		}
+		
 		header('Last-Modified: ' . $last_modified_gmt);
 		header(sprintf('ETag: "%s"', $etag));
-		// Add no-transform in order to prevent ISPs to
-		// serve image over http through a compressing proxy.
-		header('Cache-Control: public, no-transform');
+		header('Cache-Control: '. $cacheControl);
 	}
 	else {
 		$last_modified_gmt = NULL;
