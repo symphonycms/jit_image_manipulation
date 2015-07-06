016844284adaa25d4a51cba604c48a352fbf7e41
Tommaso Lanza <t@theworkers.net>
2015-07-06 11:50:08 +0100

    Listen to OPTIONS request

--- lib/image.php
+++ lib/image.php
@@ -260,9 +260,24 @@
 		$etag = NULL;
 	}
 
-	// Set cross-origin header
+	// Allow CORS
+	// respond to preflights
 	if ($settings['image']['allow_origin'] !== null) {
-		header('Access-Control-Allow-Origin: ' . $settings['image']['allow_origin']);
+		if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+			// return only the headers and not the content
+			// only allow CORS if we're doing a GET - i.e. no sending for now.
+			if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']) && $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'] == 'GET') {
+				header('Access-Control-Allow-Origin: *');
+				header('Access-Control-Allow-Headers: X-Requested-With');
+			}
+			exit;
+		} else {
+			header('Origin: ' . $settings['image']['allow_origin']);
+			header('Access-Control-Allow-Origin: ' . $settings['image']['allow_origin']);
+			header('Access-Control-Allow-Methods: GET');
+			header('Access-Control-Max-Age: 3000');
+			header('Vary: Origin, Access-Control-Request-Headers, Access-Control-Request-Method');
+		}
 	}
 
 	// Check to see if the requested image needs to be generated or if a 304
