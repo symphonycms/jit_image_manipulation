9f35b573828cbdd1115ef74ddd906115dbe32f94
nitriques <nico@nitriques.com>
2016-05-31 22:09:56 -0400

    Remove duplicate implementation
    Base class has a copy of this code

--- filters/filter.resizeandfit.php
+++ filters/filter.resizeandfit.php
@@ -12,25 +12,6 @@ class FilterResizeAndFit extends FilterResize
         );
     }
 
-    public function parseParameters($parameter_string)
-    {
-        $param = array();
-
-        if (preg_match_all('/^(1|4)\/([0-9]+)\/([0-9]+)\/(?:(0|1)\/)?(.+)$/i', $parameter_string, $matches, PREG_SET_ORDER)) {
-            $param['mode'] = (int)$matches[0][1];
-            $param['settings']['width'] = (int)$matches[0][2];
-            $param['settings']['height'] = (int)$matches[0][3];
-            $param['settings']['external'] = (bool)$matches[0][4];
-            $param['image_path'] = $matches[0][5];
-
-            if ($param['settings']['width'] == 0 && $param['settings']['height'] == 0) {
-                return false;
-            }
-        }
-
-        return !empty($param) ? $param : false;
-    }
-
     public function run(\Image $res, $settings)
     {
         $src_w = $res->Meta()->width;
