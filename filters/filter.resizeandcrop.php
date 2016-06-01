08d9970f6061feddc7f189a5f66cfacbd643ee8c
nitriques <nico@nitriques.com>
2016-05-31 20:52:07 -0400

    Extracted __calculateDestSrcXY in the parent class
    Both implementation were the same

--- filters/filter.resizeandcrop.php
+++ filters/filter.resizeandcrop.php
@@ -85,9 +85,9 @@ class FilterResizeAndCrop extends JIT\ImageFilter
         $image_height = Image::height($resource);
 
         $tmp = imagecreatetruecolor($dst_w, $dst_h);
-        self::__fill($resource, $tmp, $settings['settings']['background']);
+        static::__fill($resource, $tmp, $settings['settings']['background']);
 
-        list($src_x, $src_y, $dst_x, $dst_y) = self::__calculateDestSrcXY($dst_w, $dst_h, $image_width, $image_height, $image_width, $image_height, $settings['settings']['position']);
+        list($src_x, $src_y, $dst_x, $dst_y) = static::__calculateDestSrcXY($dst_w, $dst_h, $src_w, $src_h, $width, $height, $settings['settings']['position']);
 
         imagecopyresampled($tmp, $resource, $src_x, $src_y, $dst_x, $dst_y, $image_width, $image_height, $image_width, $image_height);
 
@@ -100,89 +100,4 @@ class FilterResizeAndCrop extends JIT\ImageFilter
         return $res;
     }
 
-    protected static function __calculateDestSrcXY($width, $height, $src_w, $src_h, $dst_w, $dst_h, $position = self::TOP_LEFT)
-    {
-
-        $dst_x = $dst_y = 0;
-        $src_x = $src_y = 0;
-
-        if ($width < $src_w) {
-            $mx = array(
-                0,
-                ceil(($src_w * 0.5) - ($width * 0.5)),
-                $src_x = $src_w - $width
-            );
-        } else {
-            $mx = array(
-                0,
-                ceil(($width * 0.5) - ($src_w * 0.5)),
-                $src_x = $width - $src_w
-            );
-        }
-
-        if ($height < $src_h) {
-            $my = array(
-                0,
-                ceil(($src_h * 0.5) - ($height * 0.5)),
-                $src_y = $src_h - $height
-            );
-        } else {
-
-            $my = array(
-                0,
-                ceil(($height * 0.5) - ($src_h * 0.5)),
-                $src_y = $height - $src_h
-            );
-        }
-
-        switch ($position) {
-
-            case 1:
-                break;
-
-            case 2:
-                $src_x = 1;
-                break;
-
-            case 3:
-                $src_x = 2;
-                break;
-
-            case 4:
-                $src_y = 1;
-                break;
-
-            case 5:
-                $src_x = 1;
-                $src_y = 1;
-                break;
-
-            case 6:
-                $src_x = 2;
-                $src_y = 1;
-                break;
-
-            case 7:
-                $src_y = 2;
-                break;
-
-            case 8:
-                $src_x = 1;
-                $src_y = 2;
-                break;
-
-            case 9:
-                $src_x = 2;
-                $src_y = 2;
-                break;
-
-        }
-
-        $a = ($width >= $dst_w ? $mx[$src_x] : 0);
-        $b = ($height >= $dst_h ? $my[$src_y] : 0);
-        $c = ($width < $dst_w ? $mx[$src_x] : 0);
-        $d = ($height < $dst_h ? $my[$src_y] : 0);
-
-        return array($a, $b, $c, $d);
-    }
 }
