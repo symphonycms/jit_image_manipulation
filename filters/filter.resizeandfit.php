7700bc228521a8a49cc8b8524422761afe0bd2c3
nitriques <nico@nitriques.com>
2016-06-06 20:51:14 -0400

    Refactored the code to use findAspectRatioValues
    Removes code duplication yet again!

--- filters/filter.resizeandfit.php
+++ filters/filter.resizeandfit.php
@@ -14,42 +14,31 @@ class FilterResizeAndFit extends FilterResize
 
     public function run(\Image $res, $settings)
     {
-        $src_w = $res->Meta()->width;
-        $src_h = $res->Meta()->height;
+        $resource = $res->Resource();
+        $src_w = Image::width($resource);
+        $src_h = Image::height($resource);
 
-        if ($settings['settings']['height'] == 0) {
-            $ratio = ($src_h / $src_w);
-            $dst_h = round($settings['meta']['width'] * $ratio);
-            $dst_w = $settings['meta']['width'];
-        } elseif ($settings['settings']['width'] == 0) {
-            $ratio = ($src_w / $src_h);
-            $dst_w = round($settings['meta']['height'] * $ratio);
-            $dst_h = $settings['meta']['height'];
+        $width = $settings['meta']['width'];
+        $height = $settings['meta']['height'];
+
+        // We must always preserve aspect ratio.
+        // Resize accordingly
+        if ($width == 0 || $height == 0) {
+            list($dst_w, $dst_h) = static::findAspectRatioValues($width, $height, $src_w, $src_h);
         } else {
-            $dst_h = $settings['meta']['height'];
-            $dst_w = $settings['meta']['width'];
+            $src_r = ($src_w / $src_h);
+            $dst_r = ($width / $height);
+
+            if ($src_r > $dst_r) {
+                list($dst_w, $dst_h) = static::findAspectRatioValues($width, null, $src_w, $src_h);
+            } else {
+                list($dst_w, $dst_h) = static::findAspectRatioValues(null, $height, $src_w, $src_h);
+            }
         }
 
-        $src_r = ($src_w / $src_h);
-        $dst_r = ($settings['meta']['width'] / $settings['meta']['height']);
+        $settings['meta']['width'] = $dst_w;
+        $settings['meta']['height'] = $dst_h;
 
-        if ($src_h <= $dst_h && $src_w <= $dst_w) {
-            $settings['settings']['width'] = $src_w;
-            $settings['settings']['height'] = $src_h;
-
-            return parent::run($res, $settings);
-        }
-
-        if ($src_h >= $dst_h && $src_r <= $dst_r) {
-            $settings['settings']['height'] = $dst_h;
-            $res = parent::run($res, $settings);
-        }
-
-        if ($src_w >= $dst_w && $src_r >= $dst_r) {
-            $settings['settings']['width'] = $dst_w;
-            $res = parent::run($res, $settings);
-        }
-
-        return $res;
+        return parent::run($res, $settings);
     }
 }
