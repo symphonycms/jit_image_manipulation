symphonycms/jit_image_manipulation

commit f2a5c177c105bc26f7b320591bfc02f81e4fec40

Allow to set the protocol for external images
This changes makes url like
/image/1/80/80/1/https://external.domain/image.jpg

Mostly needed for sites that are https only or ones that upgrade
insecure requests

lib/class.jit.php
@@ -265,7 +265,14 @@ class JIT extends Symphony
     {
         // Fetch external images
         if ($parameters['settings']['external'] === true) {
-            $image_path = "http://{$parameters['image']}";
+            if (preg_match('/^https?:\/\/?/i', $parameters['image'])) {
+                // User agent will reduce multiple slashes (//) after the protocol.
+                // This replacement will take this fact into account
+                $image_path = preg_replace('/^(https?:)\/([^\/])(.+)$/i', '$1//$2$3', $parameters['image']);
+            }
+            else {
+                $image_path = "http://{$parameters['image']}";
+            }
 
             // Image is external, check to see that it is a trusted source
             $rules = @file(WORKSPACE . '/jit-image-manipulation/trusted-sites', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);

README.markdown
@@ -155,6 +155,10 @@ The URL then requires a sixth parameter, external, (where the fourth and fifth p
 	<img src="{$root}/image/1/80/80/1/{full/path/to/image}" />
 	                                ^ External parameter
 
+You can also include the protocol in the full path of the image. This can eliminate a redirection when requesting images from a domain that upgrade insecure requests.
+
+	<img src="{$root}/image/1/80/80/1/https://{full/path/to/image}" />
+
 ### Recipes
 
 Recipes are named rules in JIT settings which help improve security and convenience. They can be edited at the preferences page in the JIT section and are saved in `/workspace/jit-image-manipulation/recipes.php`. An image using a recipe called `thumbnail` might look like:

extension.meta.xml
@@ -27,6 +27,7 @@
 			- Removed the Image::renderOutputHeaders() method
 			- Removed support for the undocumented "Direct display" (mode 0)
 			- Added support for forced download via the ?save query string
+			- Added support to pass protocol for external images
 		</release>
 		<release version="2.0.0" date="2016-04-26" min="2.6.0" max="2.x.x">
 			- JIT image process is it's own Symphony renderer now

commit e92fe94ca2ec136c9d26bdd989bbb802f3e8817d

Fix bug introduced in f2a5c177c105bc
When checking against the rule, we must remove the protocol from the
image path since rules are stripped of their paths too.

lib/class.jit.php
@@ -368,6 +368,7 @@ class JIT extends Symphony
         // Fetch external images
         if ($parameters['settings']['external'] === true) {
             $image_path = $this->normalizeExternalImageUrl($parameters['image']);
+            $protocolLess = str_replace(array('http://', 'https://'), null, $image_path);
 
             // Image is external, check to see that it is a trusted source
             $rules = @file(WORKSPACE . '/jit-image-manipulation/trusted-sites', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
@@ -383,15 +384,15 @@ class JIT extends Symphony
                         $allowed = true;
                         break;
                     } // Wildcard after domain
-                    elseif (substr($rule, -1) == '*' && strncasecmp($parameters['image'], $rule, strlen($rule) - 1) == 0) {
+                    elseif (substr($rule, -1) == '*' && strncasecmp($protocolLess, $rule, strlen($rule) - 1) == 0) {
                         $allowed = true;
                         break;
                     } // Match the start of the rule with file path
-                    elseif (strncasecmp($rule, $parameters['image'], strlen($rule)) === 0) {
+                    elseif (strncasecmp($rule, $protocolLess, strlen($rule)) === 0) {
                         $allowed = true;
                         break;
                     } // Match subdomain wildcards
-                    elseif (substr($rule, 0, 1) == '*' && preg_match('/(' . preg_quote(substr((substr($rule, -1) == '*' ? rtrim($rule, "/*") : $rule), 2), '/') . ')/', $parameters['image'])) {
+                    elseif (substr($rule, 0, 1) == '*' && preg_match('/(' . preg_quote(substr((substr($rule, -1) == '*' ? rtrim($rule, "/*") : $rule), 2), '/') . ')/', $protocolLess)) {
                         $allowed = true;
                         break;
                     }
@@ -400,7 +401,7 @@ class JIT extends Symphony
 
             if ($allowed == false) {
                 throw new JITDomainNotAllowed(
-                    sprintf('Error: Connecting to %s is not permitted.', \General::sanitize($parameters['image']))
+                    sprintf('Error: Connecting to %s is not permitted.', \General::sanitize($image_path))
                 );
             }
 